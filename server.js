47e5e9cf08679ea4d8c43ff7e94f80f769e85323
Anurag M K <109131413+Anurag-M-K@users.noreply.github.com>
2023-03-25 09:00:37 +0530

    hosted

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ app.use('upload',express.static(path.join(__dirname,'/../uploads')))
 app.use(express.json({limit: '50mb'}));
 
  
+
 //api's
 app.use('/api',userRouter);
 app.use('/admin',adminRouter);
